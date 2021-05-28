8818f17acba66e637893fdf07dbf401b5db73d7d
Marcelo Carneiro Marques <marcelodesenv100@gmail.com>
2021-05-28 14:33:22 -0300

    Fixing ResourceExceptionHandler class

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -29,7 +29,7 @@ public class ResourceExceptionHandler {
 	}
 	
 	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request) {
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError();
 		err.setTimeStamp(Instant.now());
